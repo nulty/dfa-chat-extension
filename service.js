1c4db3ece706a0c9b2f18a1e7a88f1d82b325d26
Iain McNulty <iain@inulty.com>
2022-04-30 13:07:18 +0100

    Toggle Action icon with declarativeContent

---
+++ service.js
@@ -0,0 +1,36 @@
+// Disable the action unless the current tab is dfa
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.disable();
+
+  // asynchronously fetch the alternate action icon
+  // convert it to imagedata and pass it to  SetIcon
+  alternateIcon().then((imageData) => {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+      let exampleRule = {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostSuffix: ".dfa.ie" },
+          }),
+        ],
+        actions: [
+          new chrome.declarativeContent.ShowAction(),
+          new chrome.declarativeContent.SetIcon({ imageData }),
+        ],
+      };
+
+      let rules = [exampleRule];
+      chrome.declarativeContent.onPageChanged.addRules(rules);
+    });
+  });
+});
+
+async function alternateIcon() {
+  let response = await fetch(chrome.runtime.getURL("images/white-img-16.png"));
+  let blob = await response.blob();
+  let imageBitmap = await createImageBitmap(blob);
+  let osc = new OffscreenCanvas(imageBitmap.width, imageBitmap.height);
+  let ctx = osc.getContext("2d");
+  ctx.drawImage(imageBitmap, 0, 0);
+  let imageData = ctx.getImageData(0, 0, osc.width, osc.height);
+  return imageData;
+}
