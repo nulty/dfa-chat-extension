9b0ae892b0257b0ed40fdbfeab497725a0f3ef7c
Iain McNulty <iain@inulty.com>
2022-05-02 00:32:33 +0100

    Store formfields in local storage for refilling fields

--- popup/popup.js
+++ popup/popup.js
@@ -1,42 +1,22 @@
 const url = "https://www.dfa.ie/passports/contact/";
+const form = document.querySelector("#contact-details");
+const formFields = ["name", "queryType", "emailAddress", "applicationNumber"]
 
-// function onPage () => {
-//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formEntries = new FormData(event.target).entries();
+  const json = Object.assign(
+    ...Array.from(formEntries, ([x, y]) => ({ [x]: y })),
+  );
 
-//   console.log(tab)
-// }
+  chrome.storage.local.set({ formData: JSON.stringify(json) }, function () {
+    console.log("Value is set to " + JSON.stringify(json));
+  });
+});
 
-// let rule2 = {
-//   conditions: [
-//     new chrome.declarativeContent.PageStateMatcher({
-//       pageUrl: { hostSuffix: '.dfa.ie', pathPrefix: '/passports/contact', schemes: ['https'] },
-//       css: ["img[alt='Live chat is unavailable']"]
-//     })
-//   ],
-//   actions: [ console.log('On The Page') ]
-//   // actions: [ new chrome.declarativeContent.ShowAction() ]
-// };
-
-// chrome.runtime.onInstalled.addListener(function(_details) {
-//   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-//     chrome.declarativeContent.onPageChanged.addRules([rule2]);
-//   });
-// });
-
-// chrome.runtime.onInstalled.addListener(() => {
-//   // chrome.storage.sync.set({ color });
-//   console.log(`PASSPORT: ${uri}`)
-//   console.log('Default background color set to %cgreen', `color: ${color}`);
-// });
-
-// let element = document.querySelector('a.logging')
-let element = document.uri;
-console.log('element')
-//   .addEventListener('click', (_a) => {
-//   console.log('clicked')
-// })
-
-// chrome.runtime.sendMessage('get-user-data', (response) => {
-//   // 3. Got an asynchronous response with the data from the background
-//   console.log('received user data', response);
-// });
+chrome.storage.local.get("formData", function (storage) {
+  formFields.forEach((formField) => {
+    let data = JSON.parse(storage.formData)
+    form.querySelector(`input[name=${formField}]`).value = data[formField] || '';
+  });
+});
