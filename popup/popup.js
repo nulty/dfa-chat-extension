08c81be1df8f17fda0a98b9fd5ae48fc0dbb5b49
Iain McNulty <iain@inulty.com>
2022-05-03 22:46:45 +0100

    Toggle Running / Stopped button

--- popup/popup.js
+++ popup/popup.js
@@ -7,8 +7,14 @@ const serializeToJson = (form) => {
 };
 const url = "https://www.dfa.ie/passports/contact/";
 const form = document.querySelector("#contact-details");
-const formFields = ["name", "queryType", "emailAddress", "applicationNumber"]
 const stopLink = document.querySelector("#stop");
+const startLink = document.querySelector("#start");
+const formFields = ["name", "queryType", "emailAddress", "applicationNumber"];
+
+// Initialize on/off buttons
+chrome.storage.local.get("enabled", (r) => {
+  toggleLink(r.enabled);
+});
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -25,13 +31,31 @@ form.addEventListener("submit", (event) => {
 // Send stop message to the service to remove the listener from
 // the page
 stopLink.addEventListener("click", (event) => {
-  event.preventDefault();
   chrome.storage.local.set({ enabled: false });
+  chrome.storage.local.get("enabled", (store) => {
+    toggleLink(store.enabled);
+  });
 });
 
+startLink.addEventListener("click", (event) => {
+  chrome.storage.local.set({ enabled: true });
+  chrome.storage.local.get("enabled", (store) => {
+    toggleLink(store.enabled);
+  });
+});
 chrome.storage.local.get("formData", function (storage) {
   formFields.forEach((formField) => {
     let data = JSON.parse(storage.formData)
     form.querySelector(`input[name=${formField}]`).value = data[formField] || '';
   });
 });
+function toggleLink(enabled) {
+  if (enabled) {
+    startLink.style.display = "none";
+    stopLink.style.display = "inline";
+  } else {
+    stopLink.style.display = "none";
+    startLink.style.display = "inline";
+  }
+  chrome.action.setPopup({ popup: "popup/popup.html" });
+}
